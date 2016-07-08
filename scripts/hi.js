b7c5bfba224fa64d7436ace3c01e5bfea9362374
Nick Virden <nickvirden@gmail.com>
2016-07-08 02:14:41 +0000

    New Commands for Hubot

---
+++ scripts/hi.js
@@ -0,0 +1,22 @@
+// Commands:
+//   hi - Hubot responds with "hello from hubot!"
+//   GAJS bye - Hubot responds with "goodbye from hubot!"
+
+
+module.exports = function (robot) {
+    
+    robot.hear(/hi/i, function(msg) {
+        
+        msg.send("hello from hubot!");
+        
+    });
+    
+    // You gotta say this dude's name first for it to work!
+    // In this case, it's GAJS
+    robot.respond(/bye/i, function(msg) {
+        
+        msg.send("goodbye from hubot!");
+        
+    });
+    
+}
