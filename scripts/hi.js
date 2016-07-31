7eca3cc974ebb366af394244ea4e7e47e1c61e13
Nick Virden <nickvirden@gmail>
2016-07-31 20:24:26 +0000

    Reformatting App

--- scripts/hi.js
+++ scripts/hi.js
@@ -11,8 +11,9 @@ module.exports = function (robot) {
         
     });
     
-    // You gotta say this dude's name first for it to work!
-    // In this case, it's GAJS
+    // When you're testing on the command line, you have to preface any command with the .respond method with the bot's name.
+    // In slack, you don't have to do this.
+    // Bot Name: C3P0
     robot.respond(/bye/i, function(msg) {
         
         msg.send("See ya!");
@@ -23,19 +24,50 @@ module.exports = function (robot) {
         
         msg.send("Okay. Let me find a taco recipe...");
         
-        getRequest('http://www.epicurious.com/search?terms=tacos');
+        msg.send(getURL('http://www.epicurious.com/search?terms=tacos'));
         
     });
     
-    function getRequest(url, callback) {
-        
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() { 
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-                console.log(callback(xmlHttp.responseText));
+    // Make a GET request to this URL
+    function getURL(url) {
+
+        console.log(url);
+
+        // Create a new XMLHttp Request
+        var xhr = new XMLHttpRequest();
+
+        // Once the ReadyState of the page changes, execute the function
+        // xhr.onreadystatechange = handleXHR;
+
+        // Initializes the URL using a get request
+        xhr.open('GET', url);
+
+        // Sends the GET request to the URL
+        xhr.send(null);
+    }
+
+    // Tracks the ready state of the request
+    function handleXHR() {
+
+        // If the entirety of the contents requested has been sent   
+        if (this.readyState === XMLHttpRequest.DONE) {
+
+            // If the page exists
+            if (this.status === 200) {
+
+                // Parse the JSON data into text
+                var data = JSON.parse(this.responseText);
+
+                // console.log(data);
+
+                // Print that data to the console 
+                console.log("It's " + Math.round(data.main.temp) + ". It might be " + data.weather[0].description + " in (usually) beautiful " + data.name);
+
+                // GIPHY SOLUTION: return data;
+
+            } else {
+                console.log(this.status, 'Uh oh!');
+            }
         }
-        
-        xmlHttp.open("GET", url); // true for asynchronous 
-        xmlHttp.send(null);
     }
 }
