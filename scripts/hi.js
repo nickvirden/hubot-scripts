1bc684fbe0579e337676d2388890a0a7edd15853
Nick Virden <nickvirden@gmail>
2016-07-31 20:00:31 +0000

    Reformatting App

--- scripts/hi.js
+++ scripts/hi.js
@@ -7,7 +7,7 @@ module.exports = function (robot) {
     
     robot.hear(/hi/i, function(msg) {
         
-        msg.send("hello from hubot!");
+        msg.send("Hola!");
         
     });
     
@@ -15,13 +15,27 @@ module.exports = function (robot) {
     // In this case, it's GAJS
     robot.respond(/bye/i, function(msg) {
         
-        msg.send("goodbye from hubot!");
+        msg.send("See ya!");
         
     });
     
-    robot.respond(/.*taco.*/i, function(msg) {
+    robot.respond(/.*teach.*me.*make.*taco.*/i, function(msg) {
         
-        msg.send("Yes, there's a taco.");
+        msg.send("Okay. Let me find a taco recipe...");
+        
+        getRequest('http://www.epicurious.com/search?terms=tacos');
         
     });
+    
+    function getRequest(url, callback) {
+        
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                console.log(callback(xmlHttp.responseText));
+        }
+        
+        xmlHttp.open("GET", url); // true for asynchronous 
+        xmlHttp.send(null);
+    }
 }
